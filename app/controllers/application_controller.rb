7b2f7471a33f423f34d0e23c3536b3c052629d91
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-02-20 14:23:23 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+      params[:team][:members].each do |member|
+        Superhero.new(member)
+      end
+      erb :team
+    end
 
 
 end
